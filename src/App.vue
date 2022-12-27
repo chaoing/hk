c62c313b9bddd200a6dc5e28c01025c817f854ba
lanhe233 <916039201@qq.com>
2022-12-27 16:37:12 +0800

    little modification

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,146 @@
 <template>
-  <div style="width:500px;margin-top: 50px;">
-    <img style="width: 100%" src="./assets/poster.png">
-    <div style="display: flex;align-items:center;justify-content: space-between;margin-top: 20px;">
+  <div style="width: 500px; margin-top: 50px">
+    <img style="width: 100%" src="./assets/poster.png" />
+    <div
+      style="
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-top: 20px;
+      "
+    >
       <el-select v-model="name1" placeholder="Select One">
         <el-option
-            v-for="item in nameOptions"
-            :key="item"
-            :label="item"
-            :value="item"
-            :disabled="item===name2"
+          v-for="item in nameOptions"
+          :key="item"
+          :label="item"
+          :value="item"
+          :disabled="item === name2"
         />
       </el-select>
-      <div @click="start" style="margin:0 20px;cursor: pointer;">
-        <img :class="animateClass" style="width: 100%" src="./assets/qian.png">
+      <div @click="start" style="margin: 0 20px; cursor: pointer">
+        <img
+          :class="animateClass"
+          style="width: 100%"
+          src="./assets/qian.png"
+        />
       </div>
       <el-select v-model="name2" placeholder="Select Two">
         <el-option
-            v-for="item in nameOptions"
-            :key="item"
-            :label="item"
-            :value="item"
-            :disabled="item===name1"
+          v-for="item in nameOptions"
+          :key="item"
+          :label="item"
+          :value="item"
+          :disabled="item === name1"
         />
       </el-select>
     </div>
     <div class="img-row">
-      <el-button type="warning" plain :disabled="!this.name1||!this.name2" @click="start">开始测算</el-button>
+      <el-button
+        type="warning"
+        plain
+        :disabled="!this.name1 || !this.name2"
+        @click="start"
+        >开始测算</el-button
+      >
       <el-button type="danger" plain @click="radom">随机测算</el-button>
     </div>
     <el-dialog
-        v-model="dialogVisible"
-        top="10px"
-        width="800px"
-        :before-close="handleClose">
-      <div style="margin-bottom: 30px;">
-        <div class="text">结果{{ resultData.partnership }}</div>
-        <div  class="img-row" style="display: flex;" :style="{justifyContent:resultData.shipImage?'center':'space-between'}">
-          <img class="header-img" :src="tranSrc(resultData.aimage)">
-          <img class="header-img" :src="tranSrc(resultData.bimage)">
+      v-model="dialogVisible"
+      top="0px"
+      width="800px"
+      :before-close="handleClose"
+    >
+      <div style="margin-bottom: 30px" class="cl-card">
+        <div
+          class="text cl-result"
+          :class="getPartnerShipClass(resultData.partnership)"
+        >
+          {{ resultData.partnership }}
+          <img
+            v-if="resultData.partnership === '机缘不足'"
+            class="cl-tears"
+            src="@/assets/grin-squint-tears.png"
+          />
         </div>
-        <div class="text">关系写照</div>
-        <img class="shop-img" v-if="resultData.shipImage" :src="tranSrc(resultData.shipImage)" alt="">
+        <div
+          class="img-row"
+          style="display: flex"
+          :style="{
+            justifyContent: resultData.shipImage ? 'center' : 'space-between',
+          }"
+        >
+          <img class="header-img cl-avator" :src="tranSrc(resultData.aimage)" />
+          <img class="header-img cl-avator" :src="tranSrc(resultData.bimage)" />
+        </div>
+        <!-- <div class="text">关系写照</div> -->
+        <img
+          class="shop-img cl-mt10"
+          v-if="resultData.shipImage"
+          :src="tranSrc(resultData.shipImage)"
+          alt=""
+        />
       </div>
 
       <div class="row" v-if="resultData.paAdditionalShip">
-        <div class="text">{{ resultData.paAdditionalShip.partnership }}：{{ resultData.paAdditionalShip.aname }} && {{ resultData.paAdditionalShip.bname }}</div>
-        <div class="img-row">
-          <img class="header-img" :src="tranSrc(resultData.paAdditionalShip.aimage)">
-          <img class="header-img" :src="tranSrc(resultData.paAdditionalShip.bimage)">
+        <div class="text">
+          <div>
+            {{ resultData.paAdditionalShip.aname }} &&
+            {{ resultData.paAdditionalShip.bname }}
+          </div>
+          <span class="cl-red cl-subtitle">{{
+            resultData.paAdditionalShip.partnership
+          }}</span>
         </div>
-        <div class="text">关系写照</div>
-        <img class="shop-img" v-if="resultData.paAdditionalShip.shipImage" :src="tranSrc(resultData.paAdditionalShip.shipImage)" alt="">
+        <div class="img-row">
+          <img
+            class="header-img cl-avator"
+            :src="tranSrc(resultData.paAdditionalShip.aimage)"
+          />
+          <i class="cl-love" />
+          <img
+            class="header-img"
+            :src="tranSrc(resultData.paAdditionalShip.bimage)"
+          />
+        </div>
+        <!-- <div class="text">关系写照</div> -->
+        <img
+          class="shop-img cl-mt10"
+          v-if="resultData.paAdditionalShip.shipImage"
+          :src="tranSrc(resultData.paAdditionalShip.shipImage)"
+          alt=""
+        />
       </div>
 
       <div class="row" v-if="resultData.pbAdditionalShip">
-        <div class="text">{{ resultData.pbAdditionalShip.partnership }}：{{ resultData.pbAdditionalShip.aname }} && {{ resultData.pbAdditionalShip.bname }}</div>
-        <div  class="img-row">
-          <img class="header-img" :src="tranSrc(resultData.pbAdditionalShip.aimage)">
-          <img class="header-img" :src="tranSrc(resultData.pbAdditionalShip.bimage)">
+        <div class="text">
+          <div>
+            {{ resultData.pbAdditionalShip.aname }} &&
+            {{ resultData.pbAdditionalShip.bname }}
+          </div>
+          <span class="cl-red cl-subtitle">{{
+            resultData.pbAdditionalShip.partnership
+          }}</span>
         </div>
-        <div class="text">关系写照</div>
-        <img class="shop-img" v-if="resultData.pbAdditionalShip.shipImage" :src="tranSrc(resultData.pbAdditionalShip.shipImage)" alt="">
+
+        <div class="img-row">
+          <img
+            class="header-img cl-avator"
+            :src="tranSrc(resultData.pbAdditionalShip.aimage)"
+          />
+          <i class="cl-love" />
+          <img
+            class="header-img"
+            :src="tranSrc(resultData.pbAdditionalShip.bimage)"
+          />
+        </div>
+        <!-- <div class="text">关系写照</div> -->
+        <img
+          class="shop-img"
+          v-if="resultData.pbAdditionalShip.shipImage"
+          :src="tranSrc(resultData.pbAdditionalShip.shipImage)"
+          alt=""
+        />
       </div>
     </el-dialog>
   </div>
@@ -69,107 +149,117 @@
 <script>
 /* eslint-disable */
 export default {
-  name: 'App',
+  name: "App",
   components: {},
-  data () {
+  data() {
     return {
       dialogVisible: false,
-      name1: '',
-      name2: '',
-      animateClass: '',
+      name1: "",
+      name2: "",
+      animateClass: "",
       nameOptions: [
-        '艾广华',
-        '程兵',
-        '陈国希',
-        '陈琳',
-        '陈倩倩',
-        '番自蓉',
-        '金鹏辉',
-        '康政阳',
-        '匡攀',
-        '李娟',
-        '林庆',
-        '刘暻蓓',
-        '刘颖',
-        '鲁玉兰',
-        '吕莉苹',
-        '全小珍',
-        '任金波',
-        '山坤',
-        '王平',
-        '王邵辉',
-        '王树浩',
-        '王汶玉',
-        '王雪晴',
-        '魏怡',
-        '吴隆月',
-        '谢伟',
-        '薛浩',
-        '徐蕾',
-        '徐乐曼',
-        '徐权',
-        '杨东升',
-        '杨强',
-        '严丽',
-        '廉基润',
-        '易伟',
-        '曾琴',
-        '曾涛',
-        '张德文',
-        '张涛',
-        '张栩嘉',
-        '张柱超',
-        '钟灵',
-        '朱艺丹',
-        '邹超'
+        "艾广华",
+        "程兵",
+        "陈国希",
+        "陈琳",
+        "陈倩倩",
+        "番自蓉",
+        "金鹏辉",
+        "康政阳",
+        "匡攀",
+        "李娟",
+        "林庆",
+        "刘暻蓓",
+        "刘颖",
+        "鲁玉兰",
+        "吕莉苹",
+        "全小珍",
+        "任金波",
+        "山坤",
+        "王平",
+        "王邵辉",
+        "王树浩",
+        "王汶玉",
+        "王雪晴",
+        "魏怡",
+        "吴隆月",
+        "谢伟",
+        "薛浩",
+        "徐蕾",
+        "徐乐曼",
+        "徐权",
+        "杨东升",
+        "杨强",
+        "严丽",
+        "廉基润",
+        "易伟",
+        "曾琴",
+        "曾涛",
+        "张德文",
+        "张涛",
+        "张栩嘉",
+        "张柱超",
+        "钟灵",
+        "朱艺丹",
+        "邹超",
       ],
-      resultData: {}
-    }
+      resultData: {},
+    };
   },
   methods: {
-    start () {
+    start() {
       if (this.name1 && this.name2) {
-        this.animateClass = 'ani-start'
+        this.animateClass = "ani-start";
         setTimeout(() => {
-          fetch(`/hk/ships/ce?pa=${this.name1}&pb=${this.name2}`).then(e => {
-            e.json().then(re => {
-              this.animateClass = 'ani-scale'
-              this.resultData = re.data
+          fetch(`/hk/ships/ce?pa=${this.name1}&pb=${this.name2}`).then((e) => {
+            e.json().then((re) => {
+              this.animateClass = "ani-scale";
+              this.resultData = re.data;
               setTimeout(() => {
-                this.dialogVisible = true
-              }, 1000)
-            })
-          })
-        }, 3000)
+                this.dialogVisible = true;
+              }, 1000);
+            });
+          });
+        }, 3000);
       }
     },
-    radom () {
-      let len = this.nameOptions.length
-      this.name1 = this.nameOptions[Math.floor(Math.random() * len + 1)]
-      this.name2 = this.nameOptions[Math.floor(Math.random() * len + 1)]
+    radom() {
+      let len = this.nameOptions.length;
+      this.name1 = this.nameOptions[Math.floor(Math.random() * len + 1)];
+      this.name2 = this.nameOptions[Math.floor(Math.random() * len + 1)];
       if (this.name1 === this.name2) {
-        return this.radom()
+        return this.radom();
       }
-      this.start()
+      this.start();
     },
-    handleClose () {
-      this.animateClass = ''
-      this.dialogVisible = false
+    handleClose() {
+      this.animateClass = "";
+      this.dialogVisible = false;
     },
-    tranSrc (path) {
-      return '/hk/' + path
-    }
+    tranSrc(path) {
+      return "/hk/" + path;
+    },
+    getPartnerShipClass(partnership) {
+      if (partnership.includes("不足")) {
+        return "cl-gray";
+      }
+      if (partnership.includes("缘分")) {
+        return "cl-red";
+      }
+    },
   }
-}
+};
 /* eslint-disable */
 </script>
 
 <style>
-body{
+body {
   background: url("~@/assets/bg.png");
   background-size: cover;
 }
-#app, body, html {
+#app,
+body,
+html {
   width: 100%;
   height: 100%;
   display: flex;
@@ -188,13 +278,13 @@ body{
 }
 
 .ani-start {
-  animation: AniStart .6s linear both infinite;
+  animation: AniStart 0.6s linear both infinite;
 }
 
 .ani-scale {
-  animation: AniScale .6s linear both;
+  animation: AniScale 0.6s linear both;
 }
-.img-row{
+.img-row {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -216,7 +306,6 @@ body{
     transform: rotate(0);
   }
 }
-
 
 @keyframes AniScale {
   0% {
@@ -244,8 +333,59 @@ body{
 .shop-img {
   width: 300px;
 }
-.el-dialog{
+.el-dialog {
   background: #e5cda5;
-  padding: 20px 50px;
+  padding: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.cl-result {
+  font-family: LUO;
+  font-size: 32px;
+  letter-spacing: 4px;
+  color: #522ec1;
+  /* background: url('~@/assets/ribbon.webp') no-repeat center/cover; */
+}
+
+.cl-avator {
+  border-radius: 50%;
+  border: 1px solid #9d8282;
+}
+
+.cl-card {
+  padding: 20px;
+  border-radius: 8px;
+  border: 1px solid rgb(182, 137, 137);
+}
+
+.cl-love {
+  width: 40px;
+  height: 40px;
+  margin: 0 20px;
+  background: url("~@/assets/love.png") no-repeat center/cover;
+}
+
+.cl-gray {
+  color: #584e4e;
+}
+
+.cl-red {
+  color: #e14065;
+}
+
+.cl-subtitle {
+  font-size: 16px;
+  font-weight: bold;
+  margin-top: 6px;
+}
+
+.cl-tears {
+  width: 28px;
+  height: 28px;
+}
+
+.cl-mt10 {
+  margin-top: 10px;
 }
 </style>
